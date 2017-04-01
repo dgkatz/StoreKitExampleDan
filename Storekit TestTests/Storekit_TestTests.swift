699cf4172b5461461325971ef6bacc15d1fb5eb3
Dan Katz Github Page <dgkatz@gmail.com>
2017-04-01 14:57:08 -0400

    Initial Commit

---
+++ Storekit TestTests/Storekit_TestTests.swift
@@ -0,0 +1,36 @@
+//
+//  Storekit_TestTests.swift
+//  Storekit TestTests
+//
+//  Created by Daniel Katz on 4/1/17.
+//  Copyright © 2017 Stratton Innovations. All rights reserved.
+//
+
+import XCTest
+@testable import Storekit_Test
+
+class Storekit_TestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
